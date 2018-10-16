49a2faaec4e9197389a9aaeaf8262c725cfdd47d
Zack Hall <zackhall@outlook.com>
2018-10-15 21:41:14 -0400

    wtf?

--- src/cms/preview-templates/ProductPagePreview.js
+++ src/cms/preview-templates/ProductPagePreview.js
@@ -10,15 +10,14 @@ const ProductPagePreview = ({ entry }) => {
   const gallery = entryGallery ? entryGallery.toJS() : []
 
   return (
-    // <ProductPageTemplate
-    //   title={entry.getIn(['data', 'title'])}
-    //   description={entry.getIn(['data', 'description'])}
-    //   bannerImage={entry.getIn(['data', 'bannerImage'])}
-    //   techSpecs={entry.getIn(['data', 'techSpecs'])}
-    //   finishes={finishes}
-    //   gallery={gallery}
-    // />
-    <div>Hi.</div>
+    <ProductPageTemplate
+      title={entry.getIn(['data', 'title'])}
+      description={entry.getIn(['data', 'description'])}
+      bannerImage={entry.getIn(['data', 'bannerImage'])}
+      techSpecs={entry.getIn(['data', 'techSpecs'])}
+      finishes={finishes}
+      gallery={gallery}
+    />
   )
 }
 
