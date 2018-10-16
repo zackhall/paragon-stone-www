dafa382e806073e310f1bd3632bdda65da6e31c5
Zack Hall <zackhall@outlook.com>
2018-10-15 20:53:04 -0400

    Test simple product prage preview to see if it fixes issue.

--- src/cms/preview-templates/ProductPagePreview.js
+++ src/cms/preview-templates/ProductPagePreview.js
@@ -3,21 +3,24 @@ import PropTypes from 'prop-types'
 import { ProductPageTemplate } from '../../templates/product-page'
 
 const ProductPagePreview = ({ entry }) => {
-  const entryFinishes = entry.getIn(['data', 'finishes'])
-  const finishes = entryFinishes ? entryFinishes.toJS() : []
+  // const entryFinishes = entry.getIn(['data', 'finishes'])
+  // const finishes = entryFinishes ? entryFinishes.toJS() : []
 
-  const entryGallery = entry.getIn(['data', 'gallery'])
-  const gallery = entryGallery ? entryGallery.toJS() : []
+  // const entryGallery = entry.getIn(['data', 'gallery'])
+  // const gallery = entryGallery ? entryGallery.toJS() : []
 
   return (
-    <ProductPageTemplate
-      title={entry.getIn(['data', 'title'])}
-      description={entry.getIn(['data', 'description'])}
-      bannerImage={entry.getIn(['data', 'bannerImage'])}
-      heading={entry.getIn(['data', 'heading'])}
-      finishes={finishes}
-      gallery={gallery}
-    />
+    // <ProductPageTemplate
+    //   title={entry.getIn(['data', 'title'])}
+    //   description={entry.getIn(['data', 'description'])}
+    //   bannerImage={entry.getIn(['data', 'bannerImage'])}
+    //   heading={entry.getIn(['data', 'heading'])}
+    //   finishes={finishes}
+    //   gallery={gallery}
+    // />
+    <div>
+      Hello!
+    </div>
   )
 }
 
