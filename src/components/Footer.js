4f4b92b7fd7db7b78b44a1b94042e06c1ec315be
Zack Hall <zackhall@outlook.com>
2018-10-29 23:47:38 -0400

    Dynamically grab footer links from products

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,74 +1,90 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, StaticQuery, graphql } from 'gatsby'
+import _ from 'lodash'
 
 import logobw from '../img/logo-bw.svg'
 
-const PreFooter = () => (
-  <div className="footer">
-    <section className="section has-text-white">
-      <div className="footer-logo">
-        <img src={logobw} alt="Paragon Stone Manufactured Stone Veneers Logo" />
-      </div>
-      <div className="columns">
-        <div className="column">
-          <p className="subtitle is-uppercase has-text-white">
-            Paragon Stone
-          </p>
-          <p>
-            445 South Crown Hill <br />Orrville, OH 44647
-          </p>
-        </div>
-        <div className="column">
-          <p className="subtitle is-uppercase has-text-white">
-            Contact us
-          </p>
-          <p>
-            P: (330) 930-0415<br/>
-            F: (330) 930-0416<br/>
-            E: info@paragonstone.com
-          </p>
-        </div>
-        <div className="column">
-          <p className="subtitle is-uppercase has-text-white">
-            Products
-          </p>
-          <p>
-            <Link>Prarie Stone</Link>
-          </p>
-          <p>
-            <Link>Prarie Stone</Link>
-          </p>
-          <p>
-            <Link>Prarie Stone</Link>
-          </p>
-          <p>
-            <Link>Prarie Stone</Link>
-          </p>
-          <p>
-            <Link>Prarie Stone</Link>
-          </p>
-        </div>
-        <div className="column">
-          <p className="subtitle is-uppercase has-text-white">&nbsp;</p>
-          <p>
-            <Link>Prarie Stone</Link>
-          </p>
-          <p>
-            <Link>Prarie Stone</Link>
-          </p>
-          <p>
-            <Link>Prarie Stone</Link>
-          </p>
-          <p>
-            <Link>Prarie Stone</Link>
-          </p>
-          <p>
-            <Link>Prarie Stone</Link>
-          </p>
-        </div>
-      </div>
-    </section>
-  </div>
-)
+const Footer = ({data}) => {
+  const links =
+    data && data.pages && data.pages.edges.map(page => (
+      <Link
+        to={page.node.fields.slug}
+        key={page.node.id}
+      >
+        {page.node.frontmatter.title}
+      </Link>
+    ))
 
-export default PreFooter
+  return (
+    <div className="footer">
+      <section className="section has-text-white">
+        <div className="footer-logo">
+          <img src={logobw} alt="Paragon Stone Manufactured Stone Veneers Logo" />
+        </div>
+        <div className="columns">
+          <div className="column">
+            <p className="subtitle is-uppercase has-text-white">
+              Paragon Stone
+            </p>
+            <p>
+              445 South Crown Hill <br />Orrville, OH 44647
+            </p>
+          </div>
+          <div className="column">
+            <p className="subtitle is-uppercase has-text-white">
+              Contact us
+            </p>
+            <p>
+              P: (330) 930-0415<br/>
+              F: (330) 930-0416<br/>
+              E: info@paragonstone.com
+            </p>
+          </div>
+          <div className="column">
+            <p className="subtitle is-uppercase has-text-white">
+              Products
+            </p>
+            {
+              _(links)
+                .chunk(Math.ceil(links.length / 2))
+                .first()
+            }
+          </div>
+          <div className="column">
+            <p className="subtitle is-uppercase has-text-white">&nbsp;</p>
+            {
+              _(links)
+                .chunk(Math.ceil(links.length / 2))
+                .last()
+            }
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default (props) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        pages: allMarkdownRemark(
+          filter: { frontmatter: { collectionKey: { eq: "products" } } }
+        ) {
+          edges {
+            node {
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={ data => <Footer data={data} {...props} />}
+  />
+)
