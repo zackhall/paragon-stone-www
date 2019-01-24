206a2e548dcaeab49d89f91ef4765ef0b25b467e
Zack Hall <zackhall@outlook.com>
2019-01-24 00:07:42 -0500

    Update header to point to favicon.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,10 +5,16 @@ import Navigation from '../components/Navigation'
 import PreFooter from '../components/PreFooter'
 import Footer from '../components/Footer'
 import './all.scss'
+import favicon from '../img/favicon.ico'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Paragon Stone Architectural Stone Veneers" />
+    <Helmet
+      title="Paragon Stone Architectural Stone Veneers"
+      link={[
+          { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+      ]}
+    />
     <Navigation />
     <div>{children}</div>
     <PreFooter />
